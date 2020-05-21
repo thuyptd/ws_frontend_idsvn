1a19ac1e1b8d04ce931a483287d24760c731fe6a
thuyptd <thuyptd@vn.ids.jp>
2020-05-21 10:25:41 +0700

    Update struct source

---
+++ js/script.js
@@ -0,0 +1,18 @@
+$(function () {
+    var scrollPos;
+    $('.viewFull').on('click', function () {
+        var $this = $(this),
+            pre = $this.prev();
+
+        if ($this.hasClass('expand')) {
+            $this.removeClass('expand');
+            pre.css('max-height', 500);
+            console.log(scrollPos);
+            $(window).scrollTop(scrollPos);
+        } else {
+            $this.addClass('expand');
+            pre.css('max-height', '100%');
+            scrollPos = $(window).scrollTop();
+        }
+    });
+});
