ecf8e9cd8310c37c432ff2363952be18f5c8bb06
thuyptd <thuyptd@vn.ids.jp>
2020-06-03 09:23:04 +0700

    Update content for html & javascript

---
+++ js/part/sidebar.js
@@ -0,0 +1,39 @@
+document.write('<aside class="site__sidebar">\n' +
+    '    <!--icon toggle sidebar-->\n' +
+    '    <div id="ham"></div>\n' +
+    '    <!--icon toggle sidebar-->\n' +
+    '\n' +
+    '    <h1 class="logo"><a href="https://vn.ids.jp/"><img src="'+ path_image + 'img/ids.png" width="100" alt="IDS VIETNAM Logo"/></a></h1>\n' +
+    '    <h3 class="ttl">Menu</h3>\n' +
+    '    <ul class="menu">\n' +
+    '        <li class="menu__item">\n' +
+    '            <a href="#foreword"><img src="'+ path_image +'img/svg/home-solid.svg" alt="Foreword">Foreword</a>\n' +
+    '        </li>\n' +
+    '        <li class="menu__item">\n' +
+    '            <a href="#htmlSection"><img src="'+ path_image +'img/svg/html5-brands.svg" alt="html">HTML</a>\n' +
+    '            <ul class="menu__sub">\n' +
+    '                <li><a href="#">Introduce</a></li>\n' +
+    '                <li><a href="#">Note</a></li>\n' +
+    '            </ul>\n' +
+    '        </li>\n' +
+    '\n' +
+    '\n' +
+    '        <li class="menu__item">\n' +
+    '            <a href="#cssSection"><img src="'+ path_image +'img/svg/css3-alt-brands.svg" alt="Foreword">CSS</a>\n' +
+    '            <ul class="menu__sub">\n' +
+    '                <li class="menu__sub-item"><a href="#"></a></li>\n' +
+    '                <li class="menu__sub-item"><a href="#"></a></li>\n' +
+    '\n' +
+    '            </ul>\n' +
+    '        </li>\n' +
+    '        <li class="menu__item">\n' +
+    '            <a href="#javascriptSection"><img src="'+ path_image +'img/svg/js-brands.svg" alt="Foreword">Javascript</a>\n' +
+    '        </li>\n' +
+    '        <li class="menu__item">\n' +
+    '            <a href="#UIUX"><img src="'+ path_image +'img/ui.jpg" alt="Foreword">UI-UX</a>\n' +
+    '        </li>\n' +
+    '        <li class="menu__item">\n' +
+    '            <a href="#fqa"><img src="'+ path_image +'img/svg/question-circle-solid.svg" alt="Foreword">FQA</a>\n' +
+    '        </li>\n' +
+    '    </ul>\n' +
+    '</aside>');
